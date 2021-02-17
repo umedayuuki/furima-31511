c084605278724b1b87ba188e15a304411f33875b
umedayuuki <ume1017da@icloud.com>
2021-02-18 00:19:27 +0900

    商品出品機能の実装

---
+++ spec/factories/items.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :item do
+    item_name { '商品' }
+    price { 1000 }
+    description { '説明' }
+    category_id { 2 }
+    states_id { 2 }
+    bueden_id { 2 }
+    area_id { 2 }
+    day_id { 2 }
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/hero.jpg'), filename: 'hero.jpg')
+    end
+    association :user
+  end
+end

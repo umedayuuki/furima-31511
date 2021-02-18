d302affc0fd40d826b262ecb7a70583b918b8186
umedayuuki <ume1017da@icloud.com>
2021-02-18 21:31:01 +0900

    javascriptの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,14 +2,11 @@ window.addEventListener('input', () => {
 
   const priceInput = document.getElementById("item-price");
   const inputValue = priceInput.value;
-  console.log(inputValue);
 
   const addTaxDom = document.getElementById("add-tax-price")
   addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-  console.log(addTaxDom);
 
   const profitNumber = document.getElementById("profit")
   profitNumber.innerHTML = (Math.floor(inputValue - inputValue * 0.1));
-  console.log(profitNumber);
 
 });

ea451794c45d9b8b2d89e86767b73979ca2371cd
umedayuuki <ume1017da@icloud.com>
2021-02-20 18:16:13 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
-    @items.order(id: "DESC")
+    @items = Item.all.order(id: "DESC")
   end
 
   def new
